792a7d4c3202cfd5aef94d72f3d0bacb9145e302
Paras Gupta <parasgupta4494@gmail.com>
2025-12-25 21:19:00 +0530

    done

---
+++ Addition_program.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            Addition Program
+        </title>
+    </head>
+    <body>
+        <h1>Addition Program</h1>
+    <?php
+        $a=100;
+        $b=200;
+        $c=$a+$b;
+
+        echo "Addition of ₹".$a." and ₹".$b." is: ₹".$c;
+
+    ?>
+    </body>
+</html>
---
+++ Dot_Operator.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Dot Operator Example</title>
+    </head>
+    <body>
+    <center>
+        <h1>Using the Dot Operator in PHP</h1>
+        <?php
+            $a=100;
+            echo "This Is The Value: ".$a;
+        ?>
+    </center>
+    </body>
+</html>
